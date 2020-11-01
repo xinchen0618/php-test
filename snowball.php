f2de3baee8b2858c53186f639b983baa409056a4
chenxin <chen.xin@hbtown.com>
2020-11-01 19:36:51 +0800

    pb to pe

--- snowball.php
+++ snowball.php
@@ -1,16 +1,18 @@
 #!/usr/bin/env php
 <?php
 
-$pb = $argv[1];
-$market = $argv[2];
+$market = $argv[1];
+$pe = $argv[2];
 $grow = $argv[3];
 
-$base = $market / $pb;
+$profitBase = $market / $pe;
+$profit = 0;
 $year = 1;
 do {
-    $base *= (1 + $grow);
-    echo $year, "\t", $base, "\n";
-    if ($base >= $market) {
+    $profitBase *= (1 + $grow);
+    $profit += $profitBase;
+    echo $year, "\t", $profit, "\n";
+    if ($profit >= $market) {
         break;
     }
     $year++;
