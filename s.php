f8fbc6b4fd34209e1e6df3e9a1eafffd20cad434
chenxin <chen.xin@hbtown.com>
2021-06-17 17:58:56 +0800

    snow

--- s.php
+++ s.php
@@ -1,8 +1,12 @@
 #!/usr/bin/env php
 <?php
 
-$pe = $argv[1];
-$grow = $argv[2];
+$pe = $argv[1] ?? '';
+$grow = $argv[2] ?? '';
+
+if ('' === $pe || '' === $grow) {
+    exit;
+}
 
 $profitBase = 1 / $pe;
 $profit = 0;
@@ -10,11 +14,9 @@ $year = 1;
 do {
     $profitBase *= (1 + $grow);
     $profit += $profitBase;
+    echo $year,"\t", var_export($profit, true), "\n";
     if ($profit >= 1) {
         break;
     }
     $year++;
 } while (true);
-
-echo $year, "\n";
-
