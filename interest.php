4942dea784d39a7b64abcd55303c2a85acf1d8ff
chenxin <chen.xin@hbtown.com>
2024-12-23 10:32:33 +0800

    interest

--- interest.php
+++ interest.php
@@ -4,12 +4,12 @@ $sum = 0;
 $j = 1;
 $m = 60;
 for ($i = $m; $i > 0; $i--) {
-    echo ($i * 10000) . " * x, $j", "\n";
+    echo ($i * 10000) . " * r, $j", "\n";
     $sum += $i * 10000;
     $j++;
 }
 
-echo "interest: ($m * 10000) * (0.18 / 100) * $m" . "\n";
+echo "interest = sum * r = ($m * 10000) * (0.18 / 100) * $m" . "\n";
 $interest = ($m * 10000) * (0.18 / 100) * $m;
 
 $r = $interest / $sum * 100;
